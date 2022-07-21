af7c6d6a7e36509671ee2064a22de66ea4e02c68
Simon Kelly <simonkellly@gmail.com>
2022-07-21 17:31:11 +0100

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export const Home = () => (
+  <div className="hero min-h-screen bg-base-200">
+    <div className="hero-content text-center">
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">Hello world!</h1>
+      </div>
+    </div>
+  </div>
+);
