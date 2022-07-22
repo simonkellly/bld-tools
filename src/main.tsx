aa8e4199b712f310b5ab6c3854c75136030dca0f
Simon Kelly <simonkellly@gmail.com>
2022-07-22 04:08:37 +0100

    Assign a theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { Theme } from 'react-daisyui'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './pages/Home'
 import './style/index.css'
 
+document.getElementsByTagName('html')[0].setAttribute('data-theme', 'retro');
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  
   <React.StrictMode>
     <Home />
   </React.StrictMode>
