31fbe2c8f5de78b43baba8e6366a4057773bc97e
Simon Kelly <simonkellly@gmail.com>
2022-07-22 04:23:00 +0100

    Remove unused themes from daisy

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,6 @@ module.exports = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["retro", "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    themes: ["retro"],
   },
 }
