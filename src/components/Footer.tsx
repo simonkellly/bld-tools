95abe7f6141c8186e82355e1b2166bb8a9a2c206
Simon Kelly <simonkellly@gmail.com>
2022-08-01 05:20:12 +0100

    Make footer spacing smaller

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Footer = () => (
   <footer className="footer footer-center">
-    <div className="p-4">
+    <div className="p-1">
       <a href="https://github.com/simonkellly/bld-tools/">
         <p className=" text-gray-500">
           Simon Kelly -
