848c297930b246a7bb2159a5e8498f71c9ecb25b
Simon Kelly <simonkellly@gmail.com>
2022-08-24 12:33:48 +0100

    make settings store persistant

--- src/stores/settings-store.ts
+++ src/stores/settings-store.ts
@@ -1,37 +1,46 @@
-import create from 'zustand'
+import create from "zustand";
+import { persist } from "zustand/middleware";
 
 interface SettingsState {
   helpOpen: boolean;
   openHelp: () => void;
   closeHelp: () => void;
-  
+
   showSolution: boolean;
   toggleShowSolution: () => any;
-  
+
   darkMode: boolean;
   toggleDarkMode: () => any;
-  
+
   tts: boolean;
   toggleTts: () => any;
-  
+
   volume: number;
   setVolume: (volume: number) => any;
 }
 
-export const useSettingsStore = create<SettingsState>((set) => ({
-  helpOpen: false,
-  openHelp: () => set({ helpOpen: true }),
-  closeHelp: () => set({ helpOpen: false }),
+export const useSettingsStore = create<SettingsState>()(
+  persist(
+    (set) => ({
+      helpOpen: false,
+      openHelp: () => set({ helpOpen: true }),
+      closeHelp: () => set({ helpOpen: false }),
 
-  showSolution: false,
-  toggleShowSolution: () => set((state) => ({ showSolution: !state.showSolution })),
+      showSolution: false,
+      toggleShowSolution: () =>
+        set((state) => ({ showSolution: !state.showSolution })),
 
-  darkMode: false,
-  toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
+      darkMode: false,
+      toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
 
-  tts: false,
-  toggleTts: () => set((state) => ({ tts: !state.tts })),
+      tts: false,
+      toggleTts: () => set((state) => ({ tts: !state.tts })),
 
-  volume: 50,
-  setVolume: (volume: number) => set({ volume: volume }),
-}))
+      volume: 50,
+      setVolume: (volume: number) => set({ volume: volume }),
+    }),
+    {
+      name: "settings",
+    }
+  )
+);
