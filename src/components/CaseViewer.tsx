f57c8e43d025fb7054291746ce42a3b6d5690253
Simon Kelly <simonkellly@gmail.com>
2022-09-11 14:42:34 +0100

    Add pressing enter to complete case

--- src/components/CaseViewer.tsx
+++ src/components/CaseViewer.tsx
@@ -40,6 +40,11 @@ export const CaseViewer = () => {
         e.preventDefault();
         setShowAlg(false);
       }
+
+      if (e.key == "Enter") {
+        e.preventDefault();
+        caseContext.completeCase && caseContext.completeCase();
+      }
     }
   
     document.body.addEventListener('keydown', handleDown);
